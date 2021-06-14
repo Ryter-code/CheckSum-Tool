4dce15f1ae29e1c382006b947490a7b8da63b7f7
Ryter-code <aazz99770@gmail.com>
2021-06-14 15:06:15 +0800

    First commit

---
+++ CheckSum_Tool/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CheckSum_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(419, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Checksum";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(392, 169);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Drag and drop files to here...";
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(15, 197);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(392, 29);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(419, 91);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 63);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Copy Checksum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(419, 166);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 63);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 244);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "CheckSum Tool v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CheckSum_Tool/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CheckSum_Tool
+{
+    public partial class Form1 : Form
+    {
+
+        String file_path = "None";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+
+            // check the object can be dragged to the control box
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            /**
+             * GetData() will return string[], The content is the object path,
+             * This API is allow user to drag multiple objects at once,
+             * So we always use object path[0] only.
+             */
+            string[] entriesPath = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            this.textBox1.Text = "Drag and drop files to here...\r\nGet File: " + entriesPath[0];
+            file_path = entriesPath[0];
+        }
+
+        // get checksum button 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int checksum = 0;
+            try
+            {
+                checksum = get_checksum(file_path);
+                this.textBox2.Text = "0x" + checksum.ToString("X");
+            }
+            catch (System.IO.FileNotFoundException e1)
+            {
+                MessageBox.Show("System.IO.FileNotFoundException:\nFile Not Found.", "Warning");
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("Unknown error:\n" + e2.ToString(), "Warning");
+            }
+
+        }
+
+        // exit button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            System.Environment.Exit(0);
+        }
+
+        // copy button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Windows.Forms.Clipboard.SetText(this.textBox2.Text);
+            }
+            catch(System.ArgumentNullException e1)
+            {
+                return;
+            }
+            return;
+        }
+
+        // Get Checksum
+        private int get_checksum(String file)
+        {
+            int checksum = 0;
+            string read_text = File.ReadAllText(file);
+            byte[] ASCIIbytes = Encoding.ASCII.GetBytes(read_text);
+
+            for (int i = 0; i < ASCIIbytes.Length; i++)
+            {
+                checksum += Convert.ToInt32(ASCIIbytes[i]);
+            }
+            return checksum;
+        }
+
+        // initall windows form location
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            /**
+             *    x = Windows Width - App Width
+             *    y = Windows Height - App Height
+             *     -------------------------------------
+             *     |                                           |
+             *     |                                           |
+             *     |                            (x,y)-------|
+             *     |                              |            | 
+             *     |                              |            | 
+             *     -------------------------------------
+             */
+
+            int x = (System.Windows.Forms.SystemInformation.WorkingArea.Width - this.Size.Width);
+            int y = (System.Windows.Forms.SystemInformation.WorkingArea.Height - this.Size.Height);
+
+            // Setup windows form position is control by Location
+            this.StartPosition = FormStartPosition.Manual;
+            // Setup windows form position to (x,y)
+            this.Location = (Point)new Size(x, y);
+        }
+    }
+}
